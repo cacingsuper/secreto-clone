6673afa86affbaef60066d3b13c9b2f4dae339a6
cacingsuper <cacingzuper@gmail.com>
2021-04-01 04:39:14 +0700

    updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,25 +4,25 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        Providers.Email({
-            server: {
-                host: process.env.NODEMAILER_HOST,
-                port: process.env.NODEMAILER_PORT,
-                auth: {
-                    user: process.env.NODEMAILER_USER,
-                    pass: process.env.NODEMAILER_PASSWORD
-                }
-            },
-            from: process.env.NODEMAILER_FROM
-        }),
+        // Providers.Email({
+        //     server: {
+        //         host: process.env.NODEMAILER_HOST,
+        //         port: process.env.NODEMAILER_PORT,
+        //         auth: {
+        //             user: process.env.NODEMAILER_USER,
+        //             pass: process.env.NODEMAILER_PASSWORD
+        //         }
+        //     },
+        //     from: process.env.NODEMAILER_FROM
+        // }),
         Providers.Twitter({
             clientId: process.env.TWITTER_CLIENT_ID,
             clientSecret: process.env.TWITTER_CLIENT_SECRET
         }),
-        Providers.GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
-        }),
+        // Providers.GitHub({
+        //     clientId: process.env.GITHUB_CLIENT_ID,
+        //     clientSecret: process.env.GITHUB_CLIENT_SECRET
+        // }),
         // ...add more providers here
     ],
 
